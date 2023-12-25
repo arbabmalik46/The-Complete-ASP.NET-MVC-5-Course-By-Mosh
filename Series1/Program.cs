6703bfc5f6d5e6467608ee4c900f09063d828eaf
arbabmalik46 <arbabmalik46@gmail.com>
2023-12-25 14:34:32 +0500

    AddedProject

---
+++ Series1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO.Enumeration;
+using System.Collections;
+using System.IO.Pipes;
+
+namespace Series1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+	}
+
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movies = new Movies
+            {
+                ID = 1,
+                Title = "James Bond"
+            };
+            //return View(movies);
+            //return new ViewResult();
+            //return Content("Hello World");
+            //return HttpNotFound();
+            return RedirectToAction("index","Home",new { page=1, sortBy = "Name"});
+        }
+    }
+}
